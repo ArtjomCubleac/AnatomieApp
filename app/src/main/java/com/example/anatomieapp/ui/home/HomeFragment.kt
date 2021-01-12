8f25da626bc35f10988433ccaf66bed47ccf051b
Artjom <artjomdc@gmail.com>
2021-01-12 15:50:42 +0100

    Results are showing

--- app/src/main/java/com/example/anatomieapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/anatomieapp/ui/home/HomeFragment.kt
@@ -6,21 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.anatomieapp.Quizzes.*
-import com.example.anatomieapp.R
-import com.example.anatomieapp.databinding.ActivityMainBinding.inflate
 import com.example.anatomieapp.databinding.FragmentHomeBinding
-import com.example.anatomieapp.databinding.FragmentLevel1Binding
+
 
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private  val homeViewModel: HomeViewModel by viewModels()
     private val results = arrayListOf<Results>()
     private lateinit var binding: FragmentHomeBinding
     private val resultsAdapter = ResultsAdapter(results)
@@ -30,13 +29,7 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val textView: TextView = binding.root.findViewById(R.id.text_home)
         binding = FragmentHomeBinding.inflate(layoutInflater)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return binding.root
     }
 
@@ -44,10 +37,10 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        observeResults()
     }
 
     private fun initViews() {
-        //Initialize the recycler view with a linear layout manager, adapter
         binding.rvResults.layoutManager =
             LinearLayoutManager(
                 this.context, RecyclerView.VERTICAL, false
@@ -60,14 +53,13 @@ class HomeFragment : Fragment() {
             )
         )
 
-//        for (i in Quizzes.LEVEL1QUESTIONS.indices) {
-//            quizzes.add(
-//                Quiz(
-//                    Quizzes.LEVEL1QUESTIONS[i],
-//                    Quizzes.LEVEL1ANSWERS[i]
-//                )
-//            )
-//        }
+    }
 
+    private fun observeResults() {
+        homeViewModel.results.observe(viewLifecycleOwner, Observer { results ->
+            this.results.clear()
+            this.results.addAll(results)
+            resultsAdapter.notifyDataSetChanged()
+        })
     }
 }
